3a5f285bff5b9ed2bee0fe83d9c50cd376c7369f
João Tápparo <joaovtapparo@gmail.com>
2024-03-13 16:10:45 -0300

    Add files via upload

---
+++ brightness.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the name of your second monitor
+monitor="HDMI-2"
+
+# Check if the number of arguments is correct
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [brightness from 0.1 to 1.0]"
+    exit 1
+fi
+
+# Get the brightness value from the argument
+brightness="$1"
+
+# Set the brightness using xrandr
+xrandr --output "$monitor" --brightness "$brightness"
+
